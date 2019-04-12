8bc2bff74e1fe3593d3b5691a1f9202867236957
franslourens <franslourens86@gmail.com>
2019-04-12 10:38:00 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Bananas from "./Bananas"
+import Blink from "./Blink"
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() => navigate('Profile', {name: 'Jane'})}
+      />
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+  render() {
+    return (
+   
+      <View style={styles.container}>
+        <Bananas />
+        <Blink />
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Profile: {screen: ProfileScreen},
+});
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
